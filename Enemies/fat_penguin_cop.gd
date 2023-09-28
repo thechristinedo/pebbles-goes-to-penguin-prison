dcc1c4192aa4b390e66ac9655b56690c4787a515
KevinChauNguyen <kevinnguyenchau@gmail.com>
2023-09-28 16:59:24 -0500

    When entered into the detection zone, the guard follows pebbles

--- Enemies/fat_penguin_cop.gd
+++ Enemies/fat_penguin_cop.gd
@@ -1,9 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 100
+var speed = 50
+var pebbles_chase = false
+var pebbles = null
 
 
-func _physics_process(delta):
-	var motion = Vector2()
 	
+func _physics_process(delta):
+	if pebbles_chase:
+		position +=(pebbles.position - position)/speed
+		$AnimatedSprite2D.play("Idle")
+		move_and_collide(Vector2.ZERO)
+
+
+func _on_detection_radius_body_entered(body):
+	pebbles = body
+	pebbles_chase = true
+	
+
+func _on_detection_radius_body_exited(body):
+	pass
