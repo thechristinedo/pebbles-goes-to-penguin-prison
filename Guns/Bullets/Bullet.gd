7172dc8da3a69e4ba5049e7ebc325e97c233f097
Jason Roberts <jasonroberts.dev@gmail.com>
2023-09-28 15:02:01 -0500

    refactor lifetime of bullets to use timers

--- Guns/Bullets/Bullet.gd
+++ Guns/Bullets/Bullet.gd
@@ -1,14 +1,16 @@
 extends Area2D
 
 @export var speed = 1000
-@export var life_frames = 50
 @export var damage: int = 25
 
+@onready var lifetime: Timer = $Lifetime
+
+func _ready():
+	lifetime.start()
+
 func _physics_process(delta):
-	position += transform.x * speed * delta
-	life_frames -= 1
-	if life_frames == 0:
-		free()
+	if lifetime.is_stopped(): queue_free()
+	else: position += transform.x * speed * delta
 
 func _on_body_entered(body):
 	print("collided with: ", body)
