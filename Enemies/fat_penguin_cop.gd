5c8392e1303d842829b31aa7d9fe83f81ccf2aba
Christine Do <thechristinedo@gmail.com>
2023-09-28 18:14:33 -0500

    Added chasing flip direction

--- Enemies/fat_penguin_cop.gd
+++ Enemies/fat_penguin_cop.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+var player
 var speed = 50
 var pebbles_chase = false
 var pebbles = null
@@ -9,14 +9,22 @@ var pebbles = null
 	
 func _physics_process(delta):
 	if pebbles_chase:
-		position +=(pebbles.position - position)/speed
+		player = get_node("../Pebbles")
+		position += (pebbles.position - position)/speed
 		$AnimatedSprite2D.play("Idle")
+		var direction = (player.position - self.position).normalized()
+		if direction.x < 0:
+			get_node("AnimatedSprite2D").flip_h = true
+		else:
+			get_node("AnimatedSprite2D").flip_h = false
+		
 		move_and_collide(Vector2.ZERO)
 
 
 func _on_detection_radius_body_entered(body):
 	pebbles = body
 	pebbles_chase = true
+	
 	
 
 func _on_detection_radius_body_exited(body):
