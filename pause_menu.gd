12f9e8cf1e417713d6b669e9a637f41bbfddbe8f
Christian Gabriana <142938707+christiangabr@users.noreply.github.com>
2023-10-04 00:35:18 -0500

    volume menu added

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 @onready var pausemenu = $Pause
-
+@onready var volumemenu = $Volume
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -16,7 +16,6 @@ func _process(delta):
 		get_tree().paused = true
 
 
-
 func _on_resume_pressed():
 	pausemenu.visible = false
 	get_tree().paused = false
@@ -24,3 +23,13 @@ func _on_resume_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_volume_pressed():
+	pausemenu.visible = false
+	volumemenu.visible = true
+
+func _on_back_pressed():
+	volumemenu.visible = false
+	pausemenu.visible = true
+	
