e7c06475500fdebd0cb0521a285342512aef6880
vivianc125 <vchenship@gmail.com>
2023-10-07 17:15:55 -0500

    polar bear, seals, and penguin cops have the ability to shoot and slap. Created a enemy_base file to organize common functions.

---
+++ Enemies/Fat Penguin Cop/penguin_cop_slap.gd
@@ -0,0 +1,14 @@
+extends "res://Enemies/enemy_base.gd"
+
+func _get_target_name():
+	return "Pebbles"
+
+
+func _on_slap_radius_body_entered(body):
+	var damage = 10
+	if body.name == "Pebbles":
+		if not can_shoot:
+			$AnimatedSprite2D.play("slap")
+			body.take_damage(damage)
+
+
