2f7eca7fcec65c1c16acf5ef6234d654deaaba9c
Christine Do <102181038+thechristinedo@users.noreply.github.com>
2023-10-09 22:41:57 -0500

    Added storyboard slides

--- intro_cutscene.gd
+++ intro_cutscene.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play('Intro')
-	await(get_tree().create_timer(4).timeout)
+	await(get_tree().create_timer(30).timeout)
 	get_tree().change_scene_to_file("res://menu.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
