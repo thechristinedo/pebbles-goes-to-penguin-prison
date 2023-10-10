e0ef2e62db5541fd5436d9d8a4b7dda6ff07b1f0
Christine Do <102181038+thechristinedo@users.noreply.github.com>
2023-10-10 01:53:10 -0500

    Updated scene transition

--- game_over_screen.gd
+++ game_over_screen.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 func _on_play_again_pressed():
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://Levels/prison_level.tscn")
+	SceneTransition.change_scene("res://Levels/prison_level.tscn")
 
 
 func _on_quit_pressed():
