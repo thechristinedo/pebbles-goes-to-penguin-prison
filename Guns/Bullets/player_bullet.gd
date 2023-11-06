01b7ce04664124f977baf3859bacd61fb1906023
Jason Roberts <jasonroberts.dev@gmail.com>
2023-11-05 18:12:39 -0600

    added bullet impact smoke

--- Guns/Bullets/player_bullet.gd
+++ Guns/Bullets/player_bullet.gd
@@ -2,7 +2,7 @@ class_name Bullet
 extends RigidBody2D
 
 @onready var lifetime_timer: Timer = $Lifetime
-
+const impact_smoke: PackedScene = preload("res://Guns/Bullets/Effects/impact_smoke.tscn")
 @export var speed: float = 800
 @export var lifetime: float = 3
 
@@ -18,4 +18,7 @@ func _on_area_2d_body_entered(body):
 	destroy()
 
 func destroy():
+	var smoke = impact_smoke.instantiate()
+	smoke.global_position = global_position
+	get_node("/root/World/RoomManager/Room").add_child(smoke)
 	queue_free()
