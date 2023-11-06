217e04fb51dd0136069afa8306da9f71696b4fff
Jason Roberts <jasonroberts.dev@gmail.com>
2023-11-06 15:13:00 -0600

    bug fixes and penguin cop death

--- Entities/Enemies/EntityStates/EntityIdle.gd
+++ Entities/Enemies/EntityStates/EntityIdle.gd
@@ -1,12 +1,16 @@
 extends State
 
 @export var movement_component: MovementComponent
+@export var health_component: HealthComponent
 
 func pause_wander():
 	movement_component.stop_movement()
 	get_tree().create_timer(randf_range(1,2)).connect("timeout", random_wander)
 
 func random_wander():
+	if health_component.is_dead(): 
+		movement_component.stop_movement()
+		return
 	var move_direction = Vector2(randf_range(-1,1), randf_range(-1,1)).normalized()
 	movement_component.set_movement_direction(move_direction)
 	get_tree().create_timer(randf_range(1,2)).connect("timeout", pause_wander)
@@ -14,4 +18,3 @@ func random_wander():
 func enter():
 	movement_component.walking = true
 	random_wander()
-
