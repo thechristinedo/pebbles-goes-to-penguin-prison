d1386ffc33fc6fc0675fc8d34f32fb87eed620fb
Christine Do <102181038+thechristinedo@users.noreply.github.com>
2023-11-06 18:14:31 -0600

    fixed fish and bones

---
+++ Entities/Objects/barrel_bone.gd
@@ -0,0 +1,41 @@
+extends StaticBody2D
+
+@export var health: int = 100
+
+#Fish Drops
+@export var pickup_type : PackedScene
+@onready var level_parent = get_parent()
+@export var launch_speed : float = 35
+@export var launch_direction : float = 0.25
+
+#Hit Flash Shader
+@onready var sprite = $Sprite2D
+@onready var flashTimer = $FlashHitTimer
+
+func take_damage(damage: int) -> void:
+	health -= damage
+	flash()
+	print("current health: ", health)
+	if health <= 0:
+		queue_free()
+		spawn_resource()
+
+func spawn_resource():
+	var pickup_instance : PickupBone = pickup_type.instantiate() as PickupBone
+	level_parent.add_child(pickup_instance)
+	pickup_instance.position = position
+
+	var direction : Vector2 = Vector2 (
+		randf_range(-1.0, 1.0),
+		randf_range(-1.0, 1.0)
+	).normalized()
+
+	pickup_instance.launch(direction * launch_speed, launch_direction)
+
+func flash():
+	if sprite and sprite.material:
+		sprite.material.set_shader_parameter("flash_modifier", 0.7)
+		flashTimer.start()
+
+func _on_FlashHitTimer_timeout():
+	sprite.material.set_shader_parameter("flash_modifier", 0)
