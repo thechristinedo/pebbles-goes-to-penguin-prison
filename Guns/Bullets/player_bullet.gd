3f1255336da17af6e3055213f86577a360ae7d3c
Jason Roberts <jasonroberts.dev@gmail.com>
2023-11-07 14:54:35 -0600

    Revert "Merge pull request #106 from richerduong/enemy-refactor"
    This reverts commit 79d3735a2698032f64922f11b0b96ad6f7b47fa2, reversing
    changes made to 116c82206edfa2d14f160183d3212e4db4120e16.

--- Guns/Bullets/player_bullet.gd
+++ Guns/Bullets/player_bullet.gd
@@ -1,20 +1,24 @@
 class_name Bullet
 extends RigidBody2D
 
-@export var speed: float = 800
-@export var lifetime: float = 0.1
-@export var damage: float = 20.01
-
+@onready var lifetime_timer: Timer = $Lifetime
 const impact_smoke: PackedScene = preload("res://Guns/Bullets/Effects/impact_smoke.tscn")
+@export var speed: float = 800
+@export var lifetime: float = 3
+@export var damage: int = 25
+
+func _ready():
+	lifetime_timer.connect("timeout", destroy, lifetime)
+	lifetime_timer.start()
 
 func _physics_process(delta):
 	linear_velocity = Vector2.RIGHT.rotated(rotation) * speed
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Bullet": pass
-	if body.get_node_or_null("HealthComponent"):
-		var body_health_component = body.get_node("HealthComponent") as HealthComponent
-		body_health_component.take_damage(damage, rotation)
+	print("Collided with: ", body)
+	if body.has_method("take_damage"):
+		body.take_damage(damage,self)
 	destroy()
 	
 
