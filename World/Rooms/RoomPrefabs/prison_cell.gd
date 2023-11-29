0e3cd137ad67bcc33f82f1cbbd4d692683c70ec1
Jason Roberts <jasonroberts.dev@gmail.com>
2023-11-29 14:40:48 -0600

    added armory room

--- World/Rooms/RoomPrefabs/prison_cell.gd
+++ World/Rooms/RoomPrefabs/prison_cell.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var next_room_path: String = "res://World/Rooms/RoomPrefabs/starting_room.tscn"
+@export var open_door_map_texture: Texture2D
 @onready var map_sprite: Sprite2D = $MapSprite
 @onready var door_interactable_button: Sprite2D = $DoorInteractableArea/Button
 @onready var door_interactable_animation_player: AnimationPlayer = $DoorInteractableArea/AnimationPlayer
@@ -10,7 +11,7 @@ var player_in_door_area: bool = false
 func _ready():
 	door_interactable_animation_player.play("hover")
 
-func _physics_process(_delta):	
+func _physics_process(_delta):
 	if player_in_door_area:
 		if Input.is_action_just_pressed("interact"):
 			next_scene_transition()
@@ -27,5 +28,5 @@ func _on_door_interactable_area_body_exited(body):
 		player_in_door_area = false
 
 func next_scene_transition() -> void:
-	map_sprite.texture = load("res://Assets/Maps/prison-cell-map.png")
+	map_sprite.texture = open_door_map_texture
 	RoomManager.switch_room(next_room_path)
