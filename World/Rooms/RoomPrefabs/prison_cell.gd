0ba41d376a8b1b833b8daad298f7bb527f72251c
Jason Roberts <jasonroberts.dev@gmail.com>
2023-11-29 22:18:04 -0600

    fixed enemy flash + room door functionality
    Door in room with enemies will open when all enemies are dead

--- World/Rooms/RoomPrefabs/prison_cell.gd
+++ World/Rooms/RoomPrefabs/prison_cell.gd
@@ -5,14 +5,23 @@ extends Node2D
 @onready var map_sprite: Sprite2D = $MapSprite
 @onready var door_interactable_button: Sprite2D = $DoorInteractableArea/Button
 @onready var door_interactable_animation_player: AnimationPlayer = $DoorInteractableArea/AnimationPlayer
+@onready var enemies_container: Node2D = $Enemies
 
 var player_in_door_area: bool = false
+var room_start_with_enemies: bool = false
+var door_opened: bool = true
 
 func _ready():
+	if enemies_container.get_child_count() != 0: 
+		room_start_with_enemies = true
+		door_opened = false
 	door_interactable_animation_player.play("hover")
 
 func _physics_process(_delta):
-	if player_in_door_area:
+	if !door_opened and room_start_with_enemies and no_more_enemies():
+		door_opened = true
+		map_sprite.texture = open_door_map_texture
+	if player_in_door_area and door_opened:
 		if Input.is_action_just_pressed("interact"):
 			next_scene_transition()
 		door_interactable_button.visible = true
@@ -30,3 +39,6 @@ func _on_door_interactable_area_body_exited(body):
 func next_scene_transition() -> void:
 	map_sprite.texture = open_door_map_texture
 	RoomManager.switch_room(next_room_path)
+
+func no_more_enemies() -> bool:
+	return enemies_container.get_child_count() == 0
