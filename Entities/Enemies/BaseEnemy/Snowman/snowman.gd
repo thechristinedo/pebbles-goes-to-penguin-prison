bd4bdf3367648a70e911b11832fbf65a1bcc2b93
andrewdieu <andrewdieu2011@gmail.com>
2023-11-29 23:20:24 -0600

    created new boss, added ninja stars and timers for bosses

--- Entities/Enemies/BaseEnemy/Snowman/snowman.gd
+++ Entities/Enemies/BaseEnemy/Snowman/snowman.gd
@@ -10,6 +10,13 @@ const spawn_point_count = 4
 const radius = 25
 
 func _ready():
+	_shoot()
+
+func _process(delta: float) -> void:
+	var new_rotation = rotater.rotation_degrees + rotate_speed * delta
+	rotater.rotation_degrees = fmod(new_rotation, 360)
+
+func _shoot():
 	var step = 2 * PI / spawn_point_count
 	for i in range(spawn_point_count):
 		var spawn_point = Node2D.new()
@@ -17,17 +24,18 @@ func _ready():
 		spawn_point.position = pos
 		spawn_point.rotation = pos.angle()
 		rotater.add_child(spawn_point)
-		
-	shoot_timer.wait_time = shooter_timer_wait_time
-	shoot_timer.start()
-
-func _process(delta: float) -> void:
-	var new_rotation = rotater.rotation_degrees + rotate_speed * delta
-	rotater.rotation_degrees = fmod(new_rotation, 360)
-
-func _on_shoot_timer_timeout():
-	for s in rotater.get_children():
-		var bullet = snowball_scene.instantiate()
-		get_tree().get_root().add_child(bullet)
-		bullet.position = s.global_position
-		bullet.rotation = s.global_rotation
+	
+	
+	# # of spirals we want to create
+	for i in range(3):
+		for s in rotater.get_children():
+			await get_tree().create_timer(0.2).timeout
+			print("shooting bullet")
+			var bullet = snowball_scene.instantiate()
+			get_tree().get_root().add_child(bullet)
+			bullet.position = s.global_position
+			bullet.rotation = s.global_rotation
+	
+	# waiting for 5 seconds before shooting again
+	await get_tree().create_timer(5).timeout
+	_shoot()
