f1ab52f3194fcf63722f75dfd2abfca563c60543
Christian Gabriana <142938707+christiangabr@users.noreply.github.com>
2023-11-29 22:26:17 -0600

    added victory screen scene

---
+++ GUI/User Interface/UI_menu_scenes/victory_screen.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+
+func _on_play_again_pressed():
+	get_tree().paused = false
+	#SceneTransition.change_scene("res://Levels/prison_level.tscn", 'dissolve')
+	get_tree().change_scene_to_file("res://World/world.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_back_to_main_menu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Menu/menu.tscn")
